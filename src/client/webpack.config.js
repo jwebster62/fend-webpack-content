97a4c6d54c0e7716d2b0a2e878c8c98dd13e27de
Joshua Webster <darkwolf62@gmail.com>
2020-07-22 18:06:21 -0500

    1

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,7 +1,14 @@
 const path = require("path")
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
     module: {
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "/src/client/views/index.html",
+                filename: "/index.html",
+            })
+        ]
         rules: [{
             test: '/\.js$/',
             exclude: /node-modules/,
